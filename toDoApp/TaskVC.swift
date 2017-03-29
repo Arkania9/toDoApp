c5c57d7732e3570c1818107760e3446b48a47c3a
Kamil <kamil@MacBook-Pro-Kamil.local>
2017-03-29 14:09:38 +0200

    TaskVC custom

---
+++ toDoApp/TaskVC.swift
@@ -0,0 +1,19 @@
+//
+//  TaskVC.swift
+//  toDoApp
+//
+//  Created by Kamil Zajac on 29.03.2017.
+//  Copyright © 2017 Kamil Zajac. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class TaskVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
